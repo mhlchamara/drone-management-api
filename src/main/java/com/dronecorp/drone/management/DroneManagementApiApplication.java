83bb4c45908294b17141901d522e482be3a4c079
mhlchamara <mhlchamara@gmail.com>
2023-02-04 16:01:58 +0530

    Initial Commit

---
+++ src/main/java/com/dronecorp/drone/management/DroneManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.dronecorp.drone.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneManagementApiApplication.class, args);
+	}
+
+}
